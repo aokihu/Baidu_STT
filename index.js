677fb9a33f1f13847bfe159adaaa741bc0fbec87
aokihu <aokihu@gmail.com>
2017-10-13 17:53:21 +0800

    rewrite continual mode

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
  * @author aokihu aokihu@gmail.com
  * @github https://github.com/aokihu/Baidu_STT
  * @license MIT
- * @version 1.0
+ * @version 1.0.10
  */
-const { URL } = require('url');
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
@@ -19,6 +18,19 @@ const fetch = require('little-fetch')
 const MAX_BUFFER_SIZE = 8192 * 32;
 const BDServiceAPIUrl = 'http://vop.baidu.com/server_api/';
 
+/**
+ * Define statusl constant
+ */
+const STAT_NULL = 0;
+const STAT_READY = 1;
+const STAT_SLEEP = 2;
+const STAT_WAKE_UP = 3;
+const STAT_LISTENING = 4;
+const STAT_UPLOAD = 5;
+
+/**
+ * @class BaiduSTT
+ */
 class BaiduSTT extends EventEmitter {
   /**
    * @constructor
@@ -27,11 +39,16 @@ class BaiduSTT extends EventEmitter {
    * @param {string} secretKey App secret key
    * @param {string} language Choose your language, default is 'zh'
    * @param {boolean} recordVoice Whether save the voice
-   * @param {string} hotword Hot word
+   * @param {string} hotwords Hot word
+   * @param {string} sensitivity Snowboy sensitivity
+   * @param {string} modelFile Snowboy hot word model file
+   * @param {number} gain Snowboy gain
    * @param {boolean} continual If is 'true' it will pause, 'false' is stop recording
-   * @param {stirng} voiceRate Recoding voice rate, you can set '16000' OR '8000', but '16000' maybe is the only rate with my test
+   * @param {string} voiceRate Recoding voice rate, you can set '16000' OR '8000', but '16000' maybe is the only rate with my test
    * @param {string} voicePath Voice saved path, default is current folder
    * @param {string} voiceType Voice type, default is 'wav'
+   * @param {boolean} debug Show debug information
+   * @param {number} silenceDelay The delay of silence after speak command, default is 2s
    */
   constructor({
     appId,
@@ -39,7 +56,13 @@ class BaiduSTT extends EventEmitter {
     secretKey,
     language='zh',
     recordVoice = false,
+    debug = false,
     continual = false,
+    modelFile = './resources/Alexa.pmdl',
+    sensitivity = '0.5',
+    gain = 2.0,
+    silenceDelay = 2000,
+    hotwords = "Alexa",
     voicePath = './',
     voiceRate = '16000',
     voiceType = 'wav' }) {
@@ -50,7 +73,7 @@ class BaiduSTT extends EventEmitter {
     //
     this._ = {
       token: '', /* Baidu service session token */
-      status: 'none', /* the status of class, [none, ready, requesting] */
+      status: STAT_NULL, /* the status of class, [none, ready, requesting] */
       voiceRate, /* the voice record rate */
       voicePath, /* the voice saved path */
       voiceType, /* the voice saved file type, default is 'pcm' */
@@ -58,27 +81,49 @@ class BaiduSTT extends EventEmitter {
       secretKey, /* Baidu service app secret key, you should fetch it from your baidu console */
       appId, /* It's options */
       continual, /* PAUSE OR STOP after recording */
+      modelFile,
+      hotwords,
+      sensitivity,
+      debug,
+      silenceDelay,
+      exitOnSilence: 3, /* Mic delay emit silence signal */
       canStop: false, /* It can stop record */
       buffer: {
         size: 0,
         point: [],
       },
-      cuid: 'bdtts_client_' + Date.now(),
+      cuid: 'aokihu_client_' + Date.now(),
       lan: language,
       record: recordVoice
     };
 
-    // Snowboy Model
-    this.mode = new Models();
-
+    // Use continal model,
+    // you can speak hot word to wake up
+    if(this._.continual) {
+      // Snowboy Model
+      this.model = new Models();
+      this.model.add({
+        file: this._.modelFile,
+        sensitivity: this._.sensitivity,
+        hotwords : this._.hotwords
+      });
     
+      // Snowboy Detector
+      this.detector = new Detector({
+        resource: "./resources/common.res",
+        models: this.model,
+        audioGain: this._.gain
+      });
 
+      this._.exitOnSilence = 0;
+    }
+  
     // Init mic
     this.mic = Mic({
       rate: this._.voiceRate,
       channels: 1,
-      debug: false,
-      exitOnSilence: 3,
+      debug: this._.debug,
+      exitOnSilence: this._.exitOnSilence,
       fileType: this._.voiceType,
     });
   }
@@ -92,10 +137,11 @@ class BaiduSTT extends EventEmitter {
    */
   init() {
     const tokenSession = new Token({ apiKey: this._.apiKey, secretKey: this._.secretKey });
-    tokenSession.on('ready', (token) => {
+    tokenSession.once('ready', (token) => {
       this._.token = token;
       this._.status = 'ready';
       this.emit('ready');
+      this._.status = STAT_READY; // SET STATUS = READY
     });
     tokenSession.initToken();
   }
@@ -106,23 +152,57 @@ class BaiduSTT extends EventEmitter {
    * @description Listen for your voice
    */
   listen() {
+
     const micStream = this.mic.getAudioStream();
 
+    // Save to file
     if(this._.record){
-      const filename = `output.${this._.voiceType}`;
-      const outputFileStream = fs.createWriteStream(path.resolve(this._.voicePath, filename));
-      micStream.pipe(outputFileStream);
+      this._recordToFile(micStream);
     }
 
-    micStream.on('startComplete', this._afterStart.bind(this));
-    micStream.on('resumeComplete', this._afterStart.bind(this));
-    micStream.on('data', this._recording.bind(this));
-    micStream.on('silence', this._afterRecord.bind(this));
-
+    // Set snowboy to listen hot word
+    if(this._.continual){
+      this.detector.on('hotword', this._wakeup.bind(this));
+      this.detector.on('sound', this._listening.bind(this));
+      this.detector.on('silence', this._afterRecord.bind(this));
+      micStream.pipe(this.detector);
+      this._.status = STAT_SLEEP;
+      this.emit('sleep');
+    } else {
+      micStream.on('startComplete', this._afterStart.bind(this));
+      micStream.on('data', this._recording.bind(this));
+      micStream.on('silence', this._afterRecord.bind(this));
+      this._.status = STAT_LISTENING;
+    }
+    
+    // start recording
     this.mic.start();
-
   }
 
+  /**
+   * @private
+   * @function _recordToFile()
+   * @param {Stream} inputStream mic stream
+   * @description Save voice wave data to file
+   */
+  _recordToFile(inputStream){
+    const filename = `output.${this._.voiceType}`;
+    const outputFileStream = fs.createWriteStream(path.resolve(this._.voicePath, filename));
+    inputStream.pipe(outputFileStream);
+  }
+
+  /**
+   * @private
+   * @function _wakeup
+   * @param {number} index 
+   * @param {string} hotword 
+   * @param {Buffer} buffer 
+   */
+  _wakeup(index, hotword, buffer){
+    console.log('WAKEUP')
+    this.emit('wakeup', hotword);
+    setTimeout(() => { this._.canStop = true; }, this._.silenceDelay); 
+  }
 
   /**
    * @private
@@ -132,7 +212,7 @@ class BaiduSTT extends EventEmitter {
   _afterStart() {
     this.emit('start');
     this.emit('begin');
-    setTimeout(() => { this._.canStop = true; }, 3000);
+    setTimeout(() => { this._.canStop = true; }, this.silenceDelay);
   }
 
   /**
@@ -161,15 +241,23 @@ class BaiduSTT extends EventEmitter {
 
   /**
    * @private
+   * @param {Buffer} buffer voice buffer data
+   */
+  _listening(buffer){
+    console.log('SOUND')
+  }
+
+  /**
+   * @private
    * @function do something after record
    */
   _afterRecord() {
 
+    console.log('SILENCE')
+
     if (this._.canStop) {
       if(this._.continual){
-        this.mic.pause()
-        this._.status = 'pause';
-        this._.canStop = true;
+        
       }else{
         this.mic.stop();
         this._.status = 'stop';
--- test.js
+++ test.js
@@ -7,7 +7,12 @@ const BaiduSTT = require('./index.js');
 const bdstt = new BaiduSTT({
   apiKey: 'DAZTeCYAkuAQwtPvWDcBb9Ih',
   secretKey: 'adb28deb5103bde96bd9633dd437ff2d',
-  voiceRate: '8000'
+  voiceRate: '16000',
+  // debug:true,
+  continual: true,
+  sensitivity: "0.32",
+  modelFile:'./resources/Higelx.pmdl',
+  hotwords:'海格立斯'
  });
 
 bdstt.init();
@@ -21,6 +26,8 @@ bdstt.on('start', () => console.log('Please speak something, I\'m listening...')
 bdstt.on('stop', () => console.log('Copy that'));
 bdstt.on('upload', () => console.log('Uploading voice data...'));
 bdstt.on('timeout', () => console.log('TimeOUT'))
+bdstt.on('sleep', () => console.log('I\'m sleeping...'))
+bdstt.on('wakeup', (word) => console.log('You wake me up with ', word));
 
 bdstt.on('success', data => {
   console.log(data);
